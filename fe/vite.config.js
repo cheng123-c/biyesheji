c0e50f5fa2ec736286ee31d054b74eb3d52db693
kainanYang <602533622@qq.com>
2026-03-29 00:54:01 +0800

    cros

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -11,7 +11,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    host: '0.0.0.0',
     open: true,
+    allowedHosts:[
+	    'health.com'
+    ],
     proxy: {
       '/api': {
         target: 'http://health.com:8080',
